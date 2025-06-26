4cc40e6abb5813bcf119daa0e3272aafaacce2b9
Lana <lanalumi97@gmail.com>
2025-06-26 15:55:04 -0300

    nit fixes, design checked checkbox, scroll e position fixed

--- main.js
+++ main.js
@@ -1,90 +1,73 @@
+const URL = "http://localhost:3000/";
+
+let categories = [];
+let activeCategory = {};
+
+//busca categorias no json-server
 const getCategories = async () => {
-  const response = await fetch(
-    "http://localhost:3000/categorias?_embed=tarefas"
-  );
+  const response = await fetch(`${URL}categorias?_embed=tarefas`);
   const categories = await response.json();
 
   return categories;
 };
 
-const loadCategories = async () => {
-  const categories = await getCategories();
+//cria e retorna o elemento de categoria
+const createCategoryElement = (category) => {
+  const categoryElement = document.createElement("li");
+  const completedTasks = category.tarefas.filter(({ isChecked }) => isChecked);
+  categoryElement.innerHTML = `
+  <input type="text" value="${category.title || ""}" class="category-title">
+  <div class="category-status">${completedTasks.length}/${
+    category.tarefas.length
+  }</div>`;
+
+  if (category.active) {
+    categoryElement.classList.add("active");
+  }
+  categoryElement.classList.add("category");
+
+  categoryElement.addEventListener("click", () => {
+    const allCategories = document.querySelectorAll(".category");
+    allCategories.forEach((bt) => {
+      bt.classList.remove("active");
+      categoryElement.classList.add("active");
+      activeCategory = category;
+      loadTasks();
+    });
+  });
+
+  return categoryElement;
+};
+
+const loadCategories = () => {
   const categoriesElement = document.querySelector("#categories");
   categoriesElement.innerHTML = "";
   categories.forEach((category) => {
-    const categoryElement = document.createElement("li");
-    categoryElement.innerHTML = `
-    <input type="text" value="${category.title || ""}" class="category-title">
-    <div class="category-status">0/0</div>`;
-    const activeClass = category.active ? "active" : "";
-    if (category.active) {
-      categoryElement.classList.add(activeClass);
-    }
-    categoryElement.classList.add("category");
-    categoryElement.addEventListener("click", () => {
-      const allCategories = document.querySelectorAll(".category");
-      allCategories.forEach((bt) => {
-        bt.classList.remove("active");
-        categoryElement.classList.add("active");
-      });
-      // init();
-    });
+    const categoryElement = createCategoryElement(category);
+    // init();
 
     categoriesElement.appendChild(categoryElement);
   });
-  return categories;
-};
-const postCategory = async () => {
-  const response = await fetch(
-    "http://localhost:3000/categorias",
-    { method: "POST" },
-    { body: JSON.stringify({}) }
-  );
-  console.log(response);
 };
 
-const createCategory = async () => {
-  const createCategoryElement = await document.querySelector(
-    "#create-category-button"
-  );
-  createCategoryElement.addEventListener("click", postCategory);
-};
-
-const loadTasks = async (category) => {
-  const tasks = category.tarefas;
+const loadTasks = async () => {
+  const tasks = activeCategory.tarefas;
   const tasksElement = document.querySelector("#tasks");
+  tasksElement.innerHTML = "";
+  updateHeaderTitleElement();
+  tasks?.forEach((task) => {
+    const taskElement = createTaskElement(task);
 
-  tasks.forEach((task) => {
-    const taskElement = document.createElement("li");
-    taskElement.classList.add("task");
-    const checkboxElement = document.createElement("input");
-    checkboxElement.setAttribute("type", "checkbox");
-    checkboxElement.classList.add("task-status");
-    checkboxElement.setAttribute("id", "checkbox-" + task.id);
-    taskElement.innerHTML = `
-    <label for="checkbox-${task.id}" class="label">
-      <input id="text" class="task-name" type="text" value="${
-        task.title || ""
-      }" aria-label="título da categoria">
-
-      <img src="./assets/icons/delete-task-icon.svg">
-    </label>`;
-
-    const checkedClass = task.checked ? " checked" : "";
-    taskElement.classList.add(`task${checkedClass}`);
-    checkboxElement.addEventListener("change", () => {});
-    taskElement.prepend(checkboxElement);
     tasksElement.appendChild(taskElement);
   });
 };
 
-const postTask = async (categoryId) => {
-  const response = await fetch("http://localhost:3000/tarefas", {
-    method: "POST",
-    body: JSON.stringify({ categoriaId: categoryId }),
+const createCategory = async () => {
+  const createCategoryElement = document.querySelector("#form-create-category");
+  createCategoryElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+    postCategory;
   });
-  const data = await response.json();
-  return data;
 };
 
 const createTask = async (activeCategory) => {
@@ -94,11 +77,62 @@ const createTask = async (activeCategory) => {
   );
 };
 
+const createTaskElement = (task) => {
+  const taskElement = document.createElement("li");
+
+  taskElement.innerHTML = `
+    <input id="checkbox-${task.id}" type="checkbox" class="checkbox-hidden" ${
+    task.isChecked ? "checked" : " "
+  } >
+    <label for="checkbox-${task.id}" class="task">
+    <div class="checkbox">
+    <span class="icon">
+      <img src="./assets/icons/checked-icon.svg"
+    </span>
+    </div>
+      <input id="text-${task.id}" class="task-name" type="text" value="${
+    task.title || ""
+  }" aria-label="título da categoria">
+
+      <img src="./assets/icons/delete-task-icon.svg">
+    </label>`;
+
+  return taskElement;
+};
+
+const postCategory = async () => {
+  const response = await fetch(URL + "categorias", {
+    method: "POST",
+    body: JSON.stringify({ title: "Clique para editar" }),
+  });
+  const data = await response.json();
+  return data;
+};
+
+const updateHeaderTitleElement = () => {
+  const taskHeaderElement = document.querySelector(".tasks-category");
+  taskHeaderElement.innerHTML = activeCategory.title;
+};
+
+const postTask = async () => {
+  const response = await fetch(URL + "tarefas", {
+    method: "POST",
+    body: JSON.stringify({ categoriaId: activeCategory.id }),
+  });
+  const data = await response.json();
+  return data;
+};
+
 const init = async () => {
-  const categories = await loadCategories();
-  const activeCategory = categories.find((category) => category.active);
+  //carrega informações iniciais
+  categories = await getCategories();
+  activeCategory = categories.find(({ active }) => active) || categories[0];
+
+  //inicializando elementos
+  loadCategories();
+  loadTasks();
+
   createCategory();
-  loadTasks(activeCategory);
   createTask(activeCategory);
 };
 
